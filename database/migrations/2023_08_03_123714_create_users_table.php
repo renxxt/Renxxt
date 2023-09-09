e0acec5656df4471d825bc2ef45d8c4d2047bde6
unknown <xinling07280915@gmail.com>
2023-09-09 21:38:03 +0800

    chore: update database

--- database/migrations/2023_08_03_123714_create_users_table.php
+++ database/migrations/2023_08_03_123714_create_users_table.php
@@ -22,13 +22,16 @@ return new class extends Migration
             $table->string('phonenumber', 10)->comment("電話");
             $table->string('password', 100)->comment("密碼");
             $table->integer('positionID')->unsigned()->comment("職稱 id");
+            $table->integer('superiorID')->unsigned()->nullable()->comment("上級 id");
             $table->integer('state')->default(0)->comment("狀態(0→存在，1→已刪除)");
 
             $table->foreign('departmentID')->references('departmentID')->on('departments');
             $table->foreign('positionID')->references('positionID')->on('positions');
+            $table->foreign('superiorID')->references('userID')->on('users');
             $table->index([
                 'departmentID',
-                'positionID'
+                'positionID',
+                'superiorID'
             ]);
         });
     }
--- database/migrations/2023_08_03_123742_create_device_attributes_table.php
+++ database/migrations/2023_08_03_123742_create_device_attributes_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->increments('attributeID')->comment("屬性 id");
             $table->string('name', 100)->unique()->comment("屬性名字");
             $table->integer('display')->comment("屬性狀態(0→可見，1→隱藏，2→已刪除)");
-            $table->integer('approved_layers')->comment("批准層數");
-            $table->integer('approved_level')->comment("批准層級");
+            $table->integer('approved_layers')->default(0)->comment("批准層數");
+            $table->integer('approved_level')->default(0)->comment("批准層級");
             $table->integer('pickup_form')->default(0)->comment("是否需要取用表單(0→不需要，1→需要)");
             $table->integer('return_form')->default(0)->comment("是否需要歸還表單(0→不需要，1→需要)");
             $table->integer('companion_number')->default(0)->comment("同伴同行人數");
